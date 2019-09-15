1f52c72213f8ffbb76bc6d31b6782595408c43a3
madhavareddyk78 <madhavareddyk78@gmail.com>
2019-09-15 12:10:54 +0000

    updated DBconnect.java

---
+++ DBconnect.java
@@ -0,0 +1,5 @@
+ hai this is firt githu
+
+Create the git local repositry in local machine(laptop/Desktop), add one file (DBConnect.java) and update that file, create the github remote repository(https://github.com) and mvoe the local code to github repository.
+
+there work area,stage area,local area, remote area
